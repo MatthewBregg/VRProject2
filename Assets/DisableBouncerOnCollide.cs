0328cdde5f21c529e40c3e0cceea37ba7a1d824a
GAMES <GAMES>
2016-02-18 01:23:26 -0500

    I hate ladders now

---
+++ Assets/DisableBouncerOnCollide.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableBouncerOnCollide : MonoBehaviour {
+    public GameObject bouncer;
+    public GameObject guide;
+    public GameObject lift;
+    public bool setTo;
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void setAllActive(bool active) {
+        bouncer.SetActive(active);
+        guide.SetActive(active);
+        lift.SetActive(active);
+    }
+    public void OnTriggerEnter(Collider collider) {
+
+        if (collider.gameObject.CompareTag("Player")) {
+            setAllActive(setTo);
+
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
