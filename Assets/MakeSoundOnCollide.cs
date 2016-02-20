98524454b853a0e00ef9bc28d5310fd2ad27de86
mbregg <matthewbregg@gmail.com>
2016-02-20 01:52:08 -0500

    Bushes make noise now when you walk through them. Maybe a bit too much noise. and they make noise for a bit too long when you stand still.

--- Assets/MakeSoundOnCollide.cs
+++ Assets/MakeSoundOnCollide.cs
@@ -5,7 +5,10 @@ public class MakeSoundOnCollide : MonoBehaviour {
 
     public AudioClip crashSound;
 
-
+    public void OnTriggerEnter(Collider collision) {
+        // next line requires an AudioSource component on this gameobject
+        GetComponent<AudioSource>().PlayOneShot(crashSound);
+    }
     public void OnCollisionEnter(Collision collision) {
         // next line requires an AudioSource component on this gameobject
         GetComponent<AudioSource>().PlayOneShot(crashSound);
