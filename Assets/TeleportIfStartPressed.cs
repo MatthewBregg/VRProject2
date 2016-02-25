c7bbb59230211aaaf3e708506fc2f773dc2766d3
mbregg <matthewbregg@gmail.com>
2016-02-24 23:21:24 -0500

    Finally done
    Added a button to teleport to the guard tower, to ensure the prof gets
    to see all phases, hit the start button to warp to the guard tower and
    start the attack.
    Debug function, breaks immersion/presence, but need it to present

---
+++ Assets/TeleportIfStartPressed.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportIfStartPressed : MonoBehaviour {
+	private bool teleported = false;
+	GameObject player;
+
+
+	private CharacterController controller;
+
+	void Start(){
+		controller = GetComponent<CharacterController>();
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Submit") && !teleported) {
+			Debug.Log ("Teleport!");
+			teleported = true;
+			player.transform.position = new Vector3(231.5f,4.69f,141.28f);
+
+		}
+	}
+}
